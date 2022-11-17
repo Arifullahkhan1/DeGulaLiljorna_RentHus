97defe0654ab84e307f84986e60550959f813b86
Arifullahkhan1 <arifullahkhan@hotmail.com>
2022-11-17 21:20:13 +0100

    unmached key error fixed

--- src/bookingcomponent/Customer.jsx
+++ src/bookingcomponent/Customer.jsx
@@ -3,16 +3,13 @@ import BookingSlots from "./BookingSlots";
 import "./customer.css";
 
 const Customer = (props) => {
-  const [customers, setCustomers] = useState([
- {
+  const [customers, setCustomers] = useState(
+    [{
       id: 0,
       username: "username",
       password: "password",
       roles: ["USER"],
-    }, 
-    
-    
-  ]);
+    },]);
      
  const {customer}=props
 
@@ -28,27 +25,25 @@ const Customer = (props) => {
      
   
    const fetchCustomers = async () => {
-      let response = await fetch(`${process.env.REACT_APP_BASE_URL}/api/customer/${customer?.id}`,
-        {
+    if(customer!=null){
+      let response = await fetch(`${process.env.REACT_APP_BASE_URL}/api/customer/${customer?.id}`,{
          
           headers: {
             method: "GET",
             Authorization: `Bearer ${customer?.token}`
           }
-        );
-        let customers = [];
-        try{
-          customers = await response.json();
+          });
+       
+       
+         let customers = await response.json();
           setCustomers(customers);
           localStorage.setItem("customers", JSON.stringify(customers)); 
+       
+      
         }
-        catch(error){
-          console.log(error);
-        }
-      }
-    
     
     };
+
     fetchCustomers();
 
         
